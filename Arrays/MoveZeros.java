d31a4263debdd0650a7cd5551450468de7c29fcc
Ritesh Vaja <vajaritesh@gmail.com>
2018-03-10 13:48:55 -0800

    Move All Zeros to right end of Array

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -1,12 +1,23 @@
 package com.academy.app.array.examples;
 
+/*
+ * Move All Zeros to right end of Array. Maintain the Non Zero element order
+ * Input = { 0, 1, 0, 3, 12 }
+ * Output= { 1, 3, 12, 0, 0 }
+ */
 public class MoveZeros {
 
 	public static void main(String[] args) {
 		int[] nums = { 0, 1, 0, 3, 12 };
 		print(nums);
-		// moveZerosUnStable(nums);
+		moveZerosUnStable(nums);
+		System.out.println("Unstable:");
+		print(nums);
 		moveZerosStable(nums);
+		System.out.println("Stable with Extra Space:");
+		print(nums);
+		moveZerosStableOptimize(nums);
+		System.out.println("Stable with Optimization:");
 		print(nums);
 	}
 
@@ -27,7 +38,7 @@ public class MoveZeros {
 		}
 	}
 
-	// In-Place without maintaining non-zero elements relative paths. Stable. N^2 time Complexity
+	// In-Place without maintaining non-zero elements relative paths. Stable. N^2 time and constant space Complexity
 	private static void moveZerosStable(int[] nums) {
 		int left = 0;
 		int end = nums.length;
@@ -39,6 +50,24 @@ public class MoveZeros {
 				}
 			}
 			left++;
+		}
+	}
+
+	// In-Place without maintaining non-zero elements relative paths. Stable. N time and space Complexity
+	private static void moveZerosStableOptimize(int[] nums) {
+		int[] nonZeros = new int[nums.length];
+		int j = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nonZeros[j++] = i;
+			}
+		}
+		int i = 0;
+		for (i = 0; i < j; i++) {
+			nums[i] = nums[nonZeros[i]];
+		}
+		for (; i < nums.length; i++) {
+			nums[i] = 0;
 		}
 	}
 
