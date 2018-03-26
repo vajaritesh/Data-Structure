424e58cd0c0abf32ddd60789e933fb2453a2e72a
Ritesh Vaja <vajaritesh@gmail.com>
2018-03-25 18:33:56 -0700

    Product of Array Except self

--- Arrays/ArrayProduct.java
+++ Arrays/ArrayProduct.java
@@ -2,7 +2,7 @@ package com.academy.app.array.examples;
 
 /*
  * Product of Array Except self
- * Input = {1,2,3,4,5}		Details: {arr[1]*..arr[n], arr[0]*arr[2]*...arr[n], ...., arr[0]*arr[1]*...arr[n-1]}
+ * Input = {1,2,3,4,5}			{arr[1]*..arr[n], arr[0]*arr[2]*...arr[n], ...., arr[0]*arr[1]*...arr[n-1]}
  * Output = {120,60,40,30,24}
  */
 public class ArrayProduct {
@@ -10,37 +10,38 @@ public class ArrayProduct {
 	public static void main(String[] args) {
 		int[] nums = { 1, 2, 3, 4, 5 };
 		print(nums);
-		int[] result1 = productArrayUsingTwoArrays(nums);
+		productArrayUsingTwoArrays(nums);
 		System.out.println("Product Array using Two Arrays:");
-		print(result1);
+		print(nums);
+		int[] nums1 = { 1, 2, 3, 4, 5 };
 		System.out.println("Product Array using Single Array:");
-		int[] result2 = productArray(nums);
+		int[] result2 = productArrayUsingSingleArray(nums1);
 		print(result2);
 	}
 
 	// Using two auxiliary arrays
-	private static int[] productArrayUsingTwoArrays(int[] nums) {
-		int[] result = new int[nums.length];
-		int[] left = new int[nums.length];
-		int[] right = new int[nums.length];
-		left[0] = 1;
-		for (int i = 1; i < nums.length; i++) {
-			left[i] = nums[i - 1] * left[i - 1];
+	private static void productArrayUsingTwoArrays(int[] arr) {
+		int[] left = new int[arr.length];
+		int[] right = new int[arr.length];
+		int mul;
+		left[0] = mul = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			mul *= arr[i - 1];
+			left[i] = mul;
 		}
-		// print(left);
 
-		right[nums.length - 1] = 1;
-		for (int i = nums.length - 2; i >= 0; i--) {
-			right[i] = nums[i + 1] * right[i + 1];
+		right[arr.length - 1] = mul = 1;
+		for (int i = arr.length - 2; i >= 0; i--) {
+			mul *= arr[i + 1];
+			right[i] = mul;
 		}
-		for (int i = 0; i < nums.length; i++) {
-			result[i] = left[i] * right[i];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = left[i] * right[i];
 		}
-		return result;
-		// print(right);
 	}
 
-	private static int[] productArray(int[] nums) {
+	private static int[] productArrayUsingSingleArray(int[] nums) {
 		int[] result = new int[nums.length];
 		int left = 1;
 		for (int i = 0; i < nums.length; i++) {
