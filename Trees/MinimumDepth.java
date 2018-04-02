afe81fefb48b7cb2fc85238288e5cf53addb0367
Ritesh Vaja <vajaritesh@gmail.com>
2018-04-02 10:23:35 -0700

    Minimum Depth of Binary Tree

---
+++ Trees/MinimumDepth.java
@@ -0,0 +1,73 @@
+package com.academy.binarytree.examples;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Minimum Depth of Binary Tree
+ */
+public class MinimumDepth {
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int val) {
+			this.val = val;
+			left = right = null;
+		}
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		root.right.right = new TreeNode(6);
+		int min = minDepthRec(root);
+		System.out.println(min);
+		int minDepth = minDepthIterative(root);
+		System.out.println(minDepth);
+	}
+
+	private static int minDepthRec(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.left == null) {
+			return minDepthRec(root.right) + 1;
+		}
+		if (root.right == null) {
+			return minDepthRec(root.left) + 1;
+		}
+		return Math.min(minDepthRec(root.left), minDepthRec(root.right)) + 1;
+	}
+
+	private static int minDepthIterative(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		Queue<Integer> counts = new LinkedList<Integer>();
+		if (root != null) {
+			queue.add(root);
+			counts.add(1);
+		}
+		while (!queue.isEmpty()) {
+			TreeNode top = queue.poll();
+			int count = counts.remove();
+			if (top.left == null && top.right == null) {
+				return count;
+			}
+			if (top.left != null) {
+				queue.add(top.left);
+				counts.add(count + 1);
+			}
+			if (top.right != null) {
+				queue.add(top.right);
+				counts.add(count + 1);
+			}
+		}
+		return 0;
+	}
+
+}
